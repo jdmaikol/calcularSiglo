b21617f43cd2c681de3423c0ce2847d642445315
Maikol Moreno <jdmaikol44@gmail.com>
2022-04-19 16:25:22 -0400

    siglo.js: recibe datos datos del index.html

---
+++ js/siglo.js
@@ -0,0 +1,7 @@
+const getData = () => {
+    let day = document.getElementById("valueDay").value;
+    let month = document.getElementById("valueMonth").value;
+    let year = document.getElementById("valueYear").value;
+    
+    console.log(day + month + year);
+}
