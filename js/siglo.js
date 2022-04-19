adaf3eaa4270efa7ac4a3bff8898bbb608ca390b
Maikol Moreno <jdmaikol44@gmail.com>
2022-04-19 16:48:37 -0400

    siglo.js: verificacion y logica

--- js/siglo.js
+++ js/siglo.js
@@ -3,5 +3,46 @@ const getData = () => {
     let month = document.getElementById("valueMonth").value;
     let year = document.getElementById("valueYear").value;
     
-    console.log(day + month + year);
+    // VERIFICACION DE DIA
+
+    if(!day){
+        alert("Ingresa dia valido.");
+    } else {
+        if(day <= 0){
+            alert("Este no es un dia valido.");
+        } else if (day > 31){
+            alert("No ingresar un valor mayor a 31.");
+        }
+    }
+
+    // VERIFICACION DE MES
+
+    if(!month){
+        alert("Ingresa un mes valido.");
+    } else {
+        if(month <= 0){
+            alert("Este no es un mes valido.");
+        } else if(month > 12){
+            alert("No ingresar un mes mayor a 12.");
+        }
+    }
+
+    // VERIFICACION DE YEAR
+
+    if(!year){
+        alert("Ingresa un a;o valido.");
+    } else {
+        if(year <= 0){
+            alert("Este no es un a;o valido.");
+        }
+    }
+
+
+
+
+
+
+
+
 }
+
